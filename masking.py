a10ef332248d15ba76cbc873952534e8005fa898
wahyuadhe90 <49139263+wahyuadhe90@users.noreply.github.com>
2020-03-30 15:51:10 +0700

    Update masking.py

--- masking.py
+++ masking.py
@@ -1,29 +1,16 @@
-import cv2 as cv
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-img1 = cv.imread("s2.jpg")
 
-cvt_image = cv.cvtColor(img1,cv.COLOR_BGR2HSV_FULL)
+image = cv2.imread("image3.jpg")
 
-c1 = cvt_image[:, :, 0]
-c2 = cvt_image[:, :, 1]
-c3 = cvt_image[:, :, 2]
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+lower_blue = np.array([36, 25, 25])
+upper_blue = np.array([86, 255, 255])
 
-ret,thresh_L = cv.threshold(c1,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-ret,thresh_a = cv.threshold(c2,100,150,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-ret,thresh_b = cv.threshold(c3,0.7,1,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+image[mask>0]=(255, 255, 255)
 
-f, (ax1, ax2, ax3,ax4) = plt.subplots(1,4, figsize=(10,5))
-ax1.set_title("LAB")
-ax1.imshow(cvt_image)
-ax2.set_title("H")
-ax2.imshow(thresh_L)
-ax3.set_title("S")
-ax3.imshow(thresh_a)
-ax4.set_title("V")
-ax4.imshow(thresh_b)
-plt.show()
-
+cv2.imwrite('image7.jpg',image)
+cv2.waitKey(0)
