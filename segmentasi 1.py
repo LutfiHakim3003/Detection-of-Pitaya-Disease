cac4c0ba33666e11e3845ad323272d320d847b65
wahyuadhe90 <49139263+wahyuadhe90@users.noreply.github.com>
2020-04-03 15:29:32 +0700

    Update segmentasi 1.py

--- segmentasi 1.py
+++ segmentasi 1.py
@@ -5,10 +5,10 @@ image = cv2.imread("image3.jpg")
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-lower_blue = np.array([36, 25, 25])
-upper_blue = np.array([86, 255, 255])
+lower_green = np.array([36, 25, 25])
+upper_green = np.array([86, 255, 255])
 
-mask = cv2.inRange(hsv, lower_blue, upper_blue)
+mask = cv2.inRange(hsv, lower_green, upper_green)
 
 image[mask>0]=(255, 255, 255)
 
